c1f7552417014c8d0181c77f4fb7346c467614de
charlespressler <charlespressler@gmail.com>
2025-12-30 13:22:11 -0600

    created test records and added manual template

--- dns/cloudflare/terraform/main.tf
+++ dns/cloudflare/terraform/main.tf
@@ -19,14 +19,24 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-# Create records
-resource "cloudflare_dns_record" "test" {
+# # Create records
+# resource "cloudflare_dns_record" "resource_name" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "record-name"
+#   type    = "A"
+#   content = "1.2.3.4"
+#   ttl     = 1
+#   proxied = "false"
+#   comment = "owner=crpressler | source=manual | comment=Manually created test record"
+# }
+
+resource "cloudflare_dns_record" "tf-test" {
   zone_id = var.cloudflare_zone_id
   name    = "tf-test"
   type    = "A"
   content = "1.2.3.4"
   ttl     = 1
   proxied = "false"
-  comment = "Testing Terraform functionality"
+  comment = "owner=crpressler | source=manual | comment=Manually created test record"
 }
 
